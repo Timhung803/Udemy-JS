e982b3c0caad4c48ff2b2300c9d0dfacd7e1e800
Timhung803 <timhkt@gmail.com>
2022-04-10 02:24:33 +1200

    Learning functions and Object & Array

--- Udemy-JS.js
+++ Udemy-JS.js
@@ -1,1 +1,22 @@
-console.log('Learning JS!');
+// function
+function makeCoffee(suger, milk) {
+    var instructions = 'Boil water,';
+    instructions += ' pour into cup,';
+    instructions += ' add coffee granules,';
+    instructions += ' add ' + suger + ' spoons of suger,';
+    instructions += ' add ' + milk + '% milk.';
+    return instructions;
+}
+console.log(makeCoffee(0, 20));
+
+
+let car = {
+    color: 'red',
+    speed: 300,
+    drive: function() {
+        return 'drive';
+    }
+};
+
+let shoppingList = ['Apple', 'Orange', 'Peer'
+];
